1557f7089fb604a335f26a6e91b7b0cb8ed9b425
olcaytaner <olcay.yildiz@ozyegin.edu.tr>
2026-02-15 23:40:59 +0300

    Upgraded to nodeNext.

--- dist/NaivePosTagger.js
+++ dist/NaivePosTagger.js
@@ -1,61 +1,52 @@
-(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
-    }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports", "nlptoolkit-corpus/dist/Sentence", "./PosTaggedWord", "nlptoolkit-datastructure/dist/CounterHashMap"], factory);
-    }
-})(function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    exports.NaivePosTagger = void 0;
-    const Sentence_1 = require("nlptoolkit-corpus/dist/Sentence");
-    const PosTaggedWord_1 = require("./PosTaggedWord");
-    const CounterHashMap_1 = require("nlptoolkit-datastructure/dist/CounterHashMap");
-    class NaivePosTagger {
-        /**
-         * Test method for the Naive pos tagger. For each word, the method chooses the maximum a posterior tag from all
-         * possible tag list for that word.
-         *
-         * @param sentence Sentence to be tagged.
-         * @return Annotated (tagged) sentence.
-         */
-        posTag(sentence) {
-            let result = new Sentence_1.Sentence();
-            for (let i = 0; i < sentence.wordCount(); i++) {
-                result.addWord(new PosTaggedWord_1.PosTaggedWord(sentence.getWord(i).getName(), this.maxMap.get(sentence.getWord(i).getName())));
-            }
-            return result;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NaivePosTagger = void 0;
+const Sentence_1 = require("nlptoolkit-corpus/dist/Sentence");
+const PosTaggedWord_1 = require("./PosTaggedWord");
+const CounterHashMap_1 = require("nlptoolkit-datastructure/dist/CounterHashMap");
+class NaivePosTagger {
+    maxMap;
+    /**
+     * Test method for the Naive pos tagger. For each word, the method chooses the maximum a posterior tag from all
+     * possible tag list for that word.
+     *
+     * @param sentence Sentence to be tagged.
+     * @return Annotated (tagged) sentence.
+     */
+    posTag(sentence) {
+        let result = new Sentence_1.Sentence();
+        for (let i = 0; i < sentence.wordCount(); i++) {
+            result.addWord(new PosTaggedWord_1.PosTaggedWord(sentence.getWord(i).getName(), this.maxMap.get(sentence.getWord(i).getName())));
         }
-        /**
-         * Train method for the Naive pos tagger. The algorithm gets all possible tag list. Then counts all
-         * possible tags (with its counts) for each possible word.
-         *
-         * @param corpus Training data for the tagger.
-         */
-        train(corpus) {
-            let map = new Map();
-            for (let i = 0; i < corpus.sentenceCount(); i++) {
-                let s = corpus.getSentence(i);
-                for (let j = 0; j < s.wordCount(); j++) {
-                    let word = corpus.getSentence(i).getWord(j);
-                    if (map.has(word.getName())) {
-                        map.get(word.getName()).put(word.getTag());
-                    }
-                    else {
-                        let counterMap = new CounterHashMap_1.CounterHashMap();
-                        counterMap.put(word.getTag());
-                        map.set(word.getName(), counterMap);
-                    }
+        return result;
+    }
+    /**
+     * Train method for the Naive pos tagger. The algorithm gets all possible tag list. Then counts all
+     * possible tags (with its counts) for each possible word.
+     *
+     * @param corpus Training data for the tagger.
+     */
+    train(corpus) {
+        let map = new Map();
+        for (let i = 0; i < corpus.sentenceCount(); i++) {
+            let s = corpus.getSentence(i);
+            for (let j = 0; j < s.wordCount(); j++) {
+                let word = corpus.getSentence(i).getWord(j);
+                if (map.has(word.getName())) {
+                    map.get(word.getName()).put(word.getTag());
+                }
+                else {
+                    let counterMap = new CounterHashMap_1.CounterHashMap();
+                    counterMap.put(word.getTag());
+                    map.set(word.getName(), counterMap);
                 }
             }
-            this.maxMap = new Map();
-            for (let word of map.keys()) {
-                this.maxMap.set(word, map.get(word).max());
-            }
+        }
+        this.maxMap = new Map();
+        for (let word of map.keys()) {
+            this.maxMap.set(word, map.get(word).max());
         }
     }
-    exports.NaivePosTagger = NaivePosTagger;
-});
+}
+exports.NaivePosTagger = NaivePosTagger;
 //# sourceMappingURL=NaivePosTagger.js.map
--- dist/PosTaggedCorpus.js
+++ dist/PosTaggedCorpus.js
@@ -1,75 +1,98 @@
-(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
     }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports", "nlptoolkit-corpus/dist/Corpus", "nlptoolkit-datastructure/dist/CounterHashMap", "nlptoolkit-corpus/dist/Sentence", "fs", "./PosTaggedWord"], factory);
-    }
-})(function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    exports.PosTaggedCorpus = void 0;
-    const Corpus_1 = require("nlptoolkit-corpus/dist/Corpus");
-    const CounterHashMap_1 = require("nlptoolkit-datastructure/dist/CounterHashMap");
-    const Sentence_1 = require("nlptoolkit-corpus/dist/Sentence");
-    const fs = require("fs");
-    const PosTaggedWord_1 = require("./PosTaggedWord");
-    class PosTaggedCorpus extends Corpus_1.Corpus {
-        /**
-         * A constructor of {@link PosTaggedCorpus} which initializes the sentences of the corpus, the word list of
-         * the corpus, and all possible tags.
-         */
-        constructor(fileName) {
-            super();
-            this.tagList = new CounterHashMap_1.CounterHashMap();
-            if (fileName != undefined) {
-                let newSentence = new Sentence_1.Sentence();
-                let data = fs.readFileSync(fileName, 'utf8');
-                let lines = data.split("\n");
-                for (let line of lines) {
-                    let words = line.split(/\s/);
-                    for (let word of words) {
-                        if (word != "") {
-                            if (word.includes("/")) {
-                                let name = word.substring(0, word.lastIndexOf('/'));
-                                let tag = word.substring(word.lastIndexOf('/') + 1);
-                                let shortTag;
-                                if (tag.includes("+")) {
-                                    shortTag = tag.substring(0, tag.indexOf("+"));
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PosTaggedCorpus = void 0;
+const Corpus_1 = require("nlptoolkit-corpus/dist/Corpus");
+const CounterHashMap_1 = require("nlptoolkit-datastructure/dist/CounterHashMap");
+const Sentence_1 = require("nlptoolkit-corpus/dist/Sentence");
+const fs = __importStar(require("fs"));
+const PosTaggedWord_1 = require("./PosTaggedWord");
+class PosTaggedCorpus extends Corpus_1.Corpus {
+    tagList = new CounterHashMap_1.CounterHashMap();
+    /**
+     * A constructor of {@link PosTaggedCorpus} which initializes the sentences of the corpus, the word list of
+     * the corpus, and all possible tags.
+     */
+    constructor(fileName) {
+        super();
+        if (fileName != undefined) {
+            let newSentence = new Sentence_1.Sentence();
+            let data = fs.readFileSync(fileName, 'utf8');
+            let lines = data.split("\n");
+            for (let line of lines) {
+                let words = line.split(/\s/);
+                for (let word of words) {
+                    if (word != "") {
+                        if (word.includes("/")) {
+                            let name = word.substring(0, word.lastIndexOf('/'));
+                            let tag = word.substring(word.lastIndexOf('/') + 1);
+                            let shortTag;
+                            if (tag.includes("+")) {
+                                shortTag = tag.substring(0, tag.indexOf("+"));
+                            }
+                            else {
+                                if (tag.includes("-")) {
+                                    shortTag = tag.substring(0, tag.indexOf("-"));
                                 }
                                 else {
-                                    if (tag.includes("-")) {
-                                        shortTag = tag.substring(0, tag.indexOf("-"));
-                                    }
-                                    else {
-                                        shortTag = tag;
-                                    }
+                                    shortTag = tag;
                                 }
-                                this.tagList.put(shortTag);
-                                newSentence.addWord(new PosTaggedWord_1.PosTaggedWord(name, shortTag));
-                                if (tag == ".") {
-                                    this.addSentence(newSentence);
-                                    newSentence = new Sentence_1.Sentence();
-                                }
+                            }
+                            this.tagList.put(shortTag);
+                            newSentence.addWord(new PosTaggedWord_1.PosTaggedWord(name, shortTag));
+                            if (tag == ".") {
+                                this.addSentence(newSentence);
+                                newSentence = new Sentence_1.Sentence();
                             }
                         }
                     }
                 }
-                if (newSentence.wordCount() > 0) {
-                    this.addSentence(newSentence);
-                }
+            }
+            if (newSentence.wordCount() > 0) {
+                this.addSentence(newSentence);
             }
         }
-        /**
-         * getTagList returns all possible tags as a set.
-         *
-         * @return Set of all possible tags.
-         */
-        getTagList() {
-            return this.tagList.keys();
-        }
     }
-    exports.PosTaggedCorpus = PosTaggedCorpus;
-});
+    /**
+     * getTagList returns all possible tags as a set.
+     *
+     * @return Set of all possible tags.
+     */
+    getTagList() {
+        return this.tagList.keys();
+    }
+}
+exports.PosTaggedCorpus = PosTaggedCorpus;
 //# sourceMappingURL=PosTaggedCorpus.js.map
